17ae8b9b460677afc2dadc83a28613fda678ff8e
DarkCompet <darkcompet@gmail.com>
2023-01-13 08:12:39 +0700

    Remove obsolet methods

--- src/DkJsons.cs
+++ src/DkJsons.cs
@@ -8,27 +8,15 @@ namespace Tool.Compet.Json {
 	/// - Deserialize small string => Unity is optimal
 	/// - Deserialize large string => Newton is optimal
 	public class DkJsons {
-		/// @deprecated Use `ToJson()` instead.
-		/// Convert obj -> json.
-		public static string Obj2Json(object serializableObj, bool writeIndented = false) {
-			return JsonSerializer.Serialize(serializableObj, options: new JsonSerializerOptions { WriteIndented = writeIndented });
-		}
-
 		/// Convert obj to json string.
 		/// Each field/properties in the object should be annotated with [JsonPropertyName()] attribute
-		/// TechNote: Caller should add `where T : class` to its function to allow return nullable value.
 		public static string ToJson(object serializableObj, bool writeIndented = false) {
 			return JsonSerializer.Serialize(serializableObj, options: new JsonSerializerOptions { WriteIndented = writeIndented });
 		}
 
-		/// @deprecated Use `ToObj()` instead.
-		/// Convert json -> obj.
-		public static T? Json2Obj<T>(string json) where T : class {
-			return JsonSerializer.Deserialize<T>(json);
-		}
-
 		/// Convert json string to obj.
 		/// Each field/properties in the object should be annotated with [JsonPropertyName()] attribute
+		/// TechNote: Add `where T : class` to its function to allow return nullable value.
 		public static T? ToObj<T>(string json) where T : class {
 			return JsonSerializer.Deserialize<T>(json);
 		}
